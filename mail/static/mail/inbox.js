caace01411e25fba9c97a2de7592633242ec558c
Nurfaiz Herlambang <nurfaiz3003@gmail.com>
2021-02-12 00:53:48 +0700

    Finished viewemail function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -28,6 +28,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#view-email').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -65,12 +66,11 @@ function send_email() {
 
 function show_email(email) {
   const emaildiv = document.createElement('div');
-  emaildiv.addEventListener('click', viewemail(email.id));
   
   if (email.read === true) {
-    emaildiv.className += 'container my-1 py-2 rounded border border-secondary bg-secondary text-white';
+    emaildiv.className += 'container my-1 btn btn-secondary btn-block';
   } else {
-    emaildiv.className += 'container my-1 py-2 rounded border border-primary';
+    emaildiv.className += 'container my-1 btn btn-outline-primary btn-block';
   }
   
   emaildiv.innerHTML = `
@@ -85,11 +85,32 @@ function show_email(email) {
         ${email.timestamp}
       </div>
     </div>`;
-  document.querySelector('#emails-view').append(emaildiv);
-}
+  emaildiv.addEventListener('click', function() {
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#view-email').style.display = 'block';
 
-function viewemail(id) {
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#viewemail').style.display = 'block';
+    fetch(`emails/${email.id}`)
+    .then(response => response.json())
+    .then(emails => {
+      console.log(emails);
+      document.querySelector('#view-email').innerHTML = `
+        <div><b>From</b> : ${emails.sender}</div>
+        <div><b>To</b> : ${emails.recipients}</div>
+        <div><b>Subject</b> : ${emails.subject}</div>
+        <div><b>Timestamp</b> : ${emails.timestamp}</div>
+        <br>
+        <div><p>${emails.body}</p></div>`;
+    });
+
+    fetch(`emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        read: true
+      })
+    });
+
+    document.querySelector('#view-email').innerHTML = `${email.id}`;
+  });
+  document.querySelector('#emails-view').append(emaildiv);
 }
